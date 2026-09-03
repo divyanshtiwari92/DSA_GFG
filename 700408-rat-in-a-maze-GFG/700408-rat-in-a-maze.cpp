3c4760f5972a60bc03bda3a1a555114295a37a1a
Divyansh Tiwari <divyansh9180@gmail.com>
2026-09-03 10:49:20 +0530

    Force committed solution - Time: O(3^(n*n)), Space: O(n*n)

---
+++ 700408-rat-in-a-maze-GFG/700408-rat-in-a-maze.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  private:
+    void solve(int r, int c, vector<vector<int>>& maze, int n, 
+               vector<string>& ans, string path) {
+        // Base case: reached destination
+        if (r == n - 1 && c == n - 1) {
+            ans.push_back(path);
+            return;
+        }
+
+        // Mark current cell as visited in-place
+        maze[r][c] = 0;
+
+        // Down
+        if (r + 1 < n && maze[r + 1][c] == 1)
+            solve(r + 1, c, maze, n, ans, path + 'D');
+
+        // Left
+        if (c - 1 >= 0 && maze[r][c - 1] == 1)
+            solve(r, c - 1, maze, n, ans, path + 'L');
+
+        // Right
+        if (c + 1 < n && maze[r][c + 1] == 1)
+            solve(r, c + 1, maze, n, ans, path + 'R');
+
+        // Up
+        if (r - 1 >= 0 && maze[r - 1][c] == 1)
+            solve(r - 1, c, maze, n, ans, path + 'U');
+
+        // Backtrack: restore the cell
+        maze[r][c] = 1;
+    }
+
+  public:
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
+        vector<string> ans;
+        int n = maze.size();
+
+        if (maze[0][0] == 1 && maze[n - 1][n - 1] == 1) {
+            solve(0, 0, maze, n, ans, "");
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
