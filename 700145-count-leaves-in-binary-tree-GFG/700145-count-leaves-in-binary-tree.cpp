20478853bcfc280b2de6e824cc48fd0683b5af40
Divyansh Tiwari <divyansh9180@gmail.com>
2026-09-16 09:04:31 +0530

    Added solution - Time: O(N), Space: O(H)

---
+++ 700145-count-leaves-in-binary-tree-GFG/700145-count-leaves-in-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countLeaves(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->left == nullptr && root->right == nullptr) {
+            return 1;
+        }
+        return countLeaves(root->left) + countLeaves(root->right);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
