ea27bb5a4bbe0174823f597209deee59708a0690
Divyansh Tiwari <divyansh9180@gmail.com>
2026-09-20 11:41:11 +0530

    Added solution - Time: O(nW), Space: O(nW)

---
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int knapsack(int W, vector<int>& val, vector<int>& wt) {
+        int n = val.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+
+                // Don't take the current item
+                dp[i][w] = dp[i - 1][w];
+
+                // Take the current item if possible
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = max(
+                        dp[i][w],
+                        val[i - 1] + dp[i - 1][w - wt[i - 1]]
+                    );
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
